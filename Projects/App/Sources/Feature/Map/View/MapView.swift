8022db38cb80c1b99ad5a653d5aa0a90a03ccb8b
da-hye0 <60743139+da-hye0@users.noreply.github.com>
2024-02-07 23:03:53 +0900

    Fix: 위치허가코드 정리

--- Projects/App/Sources/Feature/Map/View/MapView.swift
+++ Projects/App/Sources/Feature/Map/View/MapView.swift
@@ -43,23 +43,14 @@ struct MapView: View {
     }
     
     func startTask() async {
-        // 위치 사용 권한 설정 확인
         let locationManager = CLLocationManager()
         let authorizationStatus = locationManager.authorizationStatus
-        // 위치 사용 권한 항상 허용되어 있음
-        if authorizationStatus == .authorizedAlways {}
-        //위치 사용 권한 앱 사용 시 허용되어 있음
-        else if authorizationStatus == .authorizedWhenInUse {}
-        // 위치 사용 권한 거부되어 있음
         if authorizationStatus == .denied {
-            // 앱 설정화면으로 이동 - 필요한지 고려해보기
             DispatchQueue.main.async {
                 UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!)
             }
         }
-        // 위치 사용 권한 대기 상태
         else if authorizationStatus == .restricted || authorizationStatus == .notDetermined {
-            // 권한 요청 팝업창
             locationManager.requestWhenInUseAuthorization()
         }
     }
