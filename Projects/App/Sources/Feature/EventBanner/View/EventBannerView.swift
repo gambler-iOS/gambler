a8beed7c9f6f31ba48a366e6182a52c20af7b1df
ahnhymy <licors214@gmail.com>
2024-02-07 17:31:03 +0900

    Feat: EventBannerViewModel 기본동작 구현 완료, EventBannerView 프로토타입 완료

--- Projects/App/Sources/Feature/EventBanner/View/EventBannerView.swift
+++ Projects/App/Sources/Feature/EventBanner/View/EventBannerView.swift
@@ -7,24 +7,57 @@
 //
 
 import SwiftUI
+import Kingfisher
 
 struct EventBannerView: View {
-    var eventBannerViewModel: EventBannerViewModel
+    @ObservedObject var eventBannerViewModel: EventBannerViewModel
+    private var banners: [EventBanner] {
+        eventBannerViewModel.eventBanners
+    }
 
     var body: some View {
         VStack {
-            NavigationLink {
-                WebView(urlToLoad: "https://www.apple.com")
-            } label: {
-                Rectangle()
-                    .fill(Color.clear)
-                    .border(Color.black)
-                    .frame(width: 300, height: 150)
-                    .overlay(
-                        Text("Event Banner")
-                            .font(.largeTitle)
-                    )
+            if let url = URL(string: banners[eventBannerViewModel.currentIndex].bannerImage) {
+                NavigationLink {
+                    WebView(urlToLoad: banners[eventBannerViewModel.currentIndex].linkURL)
+                } label: {
+                    ZStack {
+                        KFImage(url)
+                            .resizable()
+                            .frame(minWidth: 300, maxWidth: .infinity, minHeight: 400, maxHeight: 400)
+                            .scaledToFit()
+                        GeometryReader(content: { geometry in
+                            Image(systemName: "bell.fill")
+                                .foregroundStyle(.white)
+                                .position(x: geometry.size.width - 20, y: 80)
+                                .onTapGesture {
+                                    print("tab")
+                                }
+                            Text("Gambler")
+                                .foregroundStyle(.white)
+                                .bold()
+                                .position(x: 50, y: 80)
+                            HStack {
+                                ForEach(Array(0 ..< banners.count), id: \.self) { index in
+                                    Circle()
+                                        .fill(self.eventBannerViewModel.currentIndex == index ? Color.red : Color.white)
+                                        .frame(width: 10, height: 10)
+                                }
+                            }
+                            .position(x: 50, y: 370)
+                        })
+                    }
+                    .frame(maxWidth: .infinity, maxHeight: 400)
+                }
             }
+        }
+        .onAppear {
+            print("apear")
+            eventBannerViewModel.startTimer()
+        }
+        .onDisappear {
+            print("disapear")
+            eventBannerViewModel.stopTimer()
         }
     }
 }
--- Projects/App/Sources/Feature/Home/View/HomeView.swift
+++ Projects/App/Sources/Feature/Home/View/HomeView.swift
@@ -16,26 +16,28 @@ struct HomeView: View {
     var body: some View {
         NavigationStack(path: $path) {
             ScrollView {
+                EventBannerView(eventBannerViewModel: eventBannerViewModel)
                 VStack(alignment: .leading) {
-                    Text("Logo")
-                    EventBannerView(eventBannerViewModel: eventBannerViewModel)
                     HomeGamesGridView(title: "인기 게임", games: homeViewModel.popularGames)
                     HomeShopListView(path: $path, title: "인기 매장", shops: homeViewModel.popularShops)
                     HomeGamesGridView(title: "신규 게임", games: homeViewModel.newGames)
                     HomeShopListView(path: $path, title: "신규 매장", shops: homeViewModel.newShops)
                 }
+                .background(.red)
                 .padding()
-                .navigationDestination(for: Shop.self) { shop in
-                    ShopDetailView(path: $path, shop: shop)
+                .navigationDestination(for: Shop.self) { _ in
+                    ShopDetailView()
                 }
             }
+            .frame(maxWidth: .infinity)
+            .ignoresSafeArea()
         }
         // MARK: 개발 시 쓸모없는 데이터 호출을 막기 위해 firestore fetch 대신 더미데이터 사용하기 위한 주석 처리
-        .task {
-            print(" == fetch homeviewModel == ")
-            await eventBannerViewModel.fetchData()
-            await homeViewModel.fetchData()
-        }
+//        .task {
+//            print(" == fetch homeviewModel == ")
+//            await eventBannerViewModel.fetchData()
+//            await homeViewModel.fetchData()
+//        }
     }
 }
 
