0cbb65928f42b5c2b8be92b947570d12cb2a2b9e
ahnhymy <licors214@gmail.com>
2024-02-13 01:21:38 +0900

    Fix: GameCategoryCellView imageUrl, name 문자열값 받는 것으로 수정

--- Projects/App/Sources/Common/GameCategoryCellView.swift
+++ Projects/App/Sources/Common/GameCategoryCellView.swift
@@ -10,11 +10,12 @@ import SwiftUI
 import Kingfisher
 
 struct GameCategoryCellView: View {
-    let game: Game
+    let imageUrl: String
+    let name: String
 
     var body: some View {
         VStack(spacing: 16) {
-            if let url = URL(string: game.gameImage) {
+            if let url = URL(string: imageUrl) {
                 KFImage(url)
                     .resizable()
                     .frame(width: 80, height: 80)
@@ -25,7 +26,7 @@ struct GameCategoryCellView: View {
                     .frame(width: 80, height: 80)
             }
             
-            Text(game.gameName)
+            Text(name)
                 .font(.body2M)
                 .foregroundStyle(Color.gray700)
         }
@@ -33,11 +34,5 @@ struct GameCategoryCellView: View {
 }
 
 #Preview {
-    GameCategoryCellView(game: Game(id: UUID().uuidString, gameName: "아임더보스",
-                                    gameImage: "https://weefun.co.kr/shopimages/weefun/007009000461.jpg?1596805186",
-                                    gameIntroduction: GameIntroduction(difficulty: 3.1, minPlayerCount: 2,
-                                                                       maxPlayerCount: 4, playTime: 2,
-                                                                       genre: .fantasy),
-                                    descriptionImage: ["image"], gameLink: "link", createdDate: Date(),
-                                    reviewCount: 5, reviewRatingAverage: 3.5))
+    GameCategoryCellView(imageUrl: "", name: "전략")
 }
