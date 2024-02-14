833bb8ad93da9e8b20f8208d87c78f2f1195917f
Sunghun Park <dfstem9@gmail.com>
2024-02-15 02:13:44 +0900

    Feat: Report Model 추가

---
+++ Projects/App/Sources/Model/Report.swift
@@ -0,0 +1,18 @@
+//
+//  Report.swift
+//  gambler
+//
+//  Created by 박성훈 on 2/15/24.
+//  Copyright © 2024 gambler. All rights reserved.
+//
+
+import Foundation
+
+struct Report {
+    var id: String
+    let reviewId: String
+    let userId: String
+    let reportTitle: String
+    let reportContent: String
+    let reportCategory: String
+}
