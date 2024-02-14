b52eaa7a32be7f94d95b032777a741a15afda547
Sunghun Park <dfstem9@gmail.com>
2024-02-15 00:08:46 +0900

    Feat: ReportView 추가

---
+++ Projects/App/Sources/Feature/MyPage/View/ReportDetailView.swift
@@ -0,0 +1,87 @@
+//
+//  ReportDetailView.swift
+//  gambler
+//
+//  Created by 박성훈 on 2/13/24.
+//  Copyright © 2024 gambler. All rights reserved.
+//
+
+import SwiftUI
+
+struct ReportDetailView: View {
+    
+    enum Field: Hashable {
+            case reasonForReporting
+    }
+        
+    @FocusState private var focusedField: Field?
+    @State private var reportContent: String = ""
+    @State private var placeholder: String = "앱의 버그 및 신고사항을 자유롭게 적어주세요"
+    private let limitChar: Int = 100
+    
+    var body: some View {
+        VStack {
+            ZStack(alignment: .topLeading) {
+                // placeholder 구현
+                if reportContent.isEmpty {
+                    Text(placeholder)   // State 변수 선언
+                        .lineSpacing(10)
+                        .foregroundColor(Color.primary.opacity(0.25))
+                        .padding(.top, 10)
+                        .padding(.leading, 10)
+                        .zIndex(1)  // 우선순위
+                        .onTapGesture {
+                            self.focusedField = .reasonForReporting
+                        }
+                }
+                
+                // 텍스트 에디터
+                VStack {
+                    TextEditor(text: $reportContent)
+                        .keyboardType(.default)
+                        .foregroundColor(Color.black)
+                        .frame(height: 150)
+                        .lineSpacing(10)
+                        .focused($focusedField, equals: .reasonForReporting)
+                    
+                    // 글자 수 제한
+                        .onChange(of: self.reportContent, perform: {
+                            if $0.count > limitChar {
+                                self.reportContent = String($0.prefix(limitChar))
+                            }
+                        })
+                    // 텍스트에디터를 다시 누르면 키보드 내려가게 함
+                        .onTapGesture {
+                            UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+                        }
+                    
+                    VStack(alignment: .trailing) {
+                        Spacer()
+                        HStack {
+                            Spacer()
+                            Text("\(reportContent.count) / \(limitChar)")
+                        }
+                    }
+                    .padding()  // Text를 떨어뜨리기 위함
+                }
+                .border(.secondary)
+            }  // ZStack
+            
+            
+            Button {
+                /*
+                 1. 파이어베이스에 올림
+                 2. 알럿을 띄워서 신고하시겠습니까 문구
+                 3. 알럿에서 확인을 누르면 신고하기에서 나가서 마이페이지로 감
+                 */
+            } label: {
+                Text("작성 완료")
+            }
+            
+        }
+    }
+}
+
+#Preview {
+    ReportDetailView()
+}
---
+++ Projects/App/Sources/Feature/MyPage/View/ReportView.swift
@@ -0,0 +1,27 @@
+//
+//  ReportView.swift
+//  gambler
+//
+//  Created by 박성훈 on 2/13/24.
+//  Copyright © 2024 gambler. All rights reserved.
+//
+
+import SwiftUI
+
+struct ReportView: View {
+    var body: some View {
+        VStack {
+            Text("스팸")
+            Text("유료 광고 포함")
+            Text("거짓 정보")
+            Text("지식재산권 침해")
+            Text("괴롭힘 또는 개인 정보 침해")
+            Text("성인용 컨텐츠")
+            Text("기타")
+        }
+    }
+}
+
+#Preview {
+    ReportView()
+}
