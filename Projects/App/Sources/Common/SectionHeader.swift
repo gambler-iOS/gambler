7f4d4efa5b0e7319eed75b5b58a9da6faafbb011
da-hye0 <60743139+da-hye0@users.noreply.github.com>
2024-02-16 05:17:20 +0900

    Feat: SectionHeader 추가

---
+++ Projects/App/Sources/Common/SectionHeader.swift
@@ -0,0 +1,34 @@
+//
+//  SectionHeader.swift
+//  gambler
+//
+//  Created by daye on 2/7/24.
+//  Copyright © 2024 gambler. All rights reserved.
+//
+
+import SwiftUI
+
+struct SectionHeader: View {
+    var title: String
+    var rating: String?
+    
+    var body: some View {
+        HStack(spacing: 0){
+            Text(title)
+                .font(.subHead1B)
+                .foregroundStyle(Color.gray700)
+            
+            if let content = rating {
+                Text(content)
+                    .font(.body1B)
+                    .foregroundStyle(Color.primaryDefault)
+                    .padding(.leading, 8)
+           }
+            
+            Spacer()
+            GamblerAsset.arrowRight.swiftUIImage
+                .resizable()
+                .frame(width: 24, height: 24)
+        }.padding(24)
+    }
+}
