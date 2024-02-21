48cf7caec73129f3a2608cb3720a149a9fd5dcf1
da-hye0 <60743139+da-hye0@users.noreply.github.com>
2024-02-21 23:04:16 +0900

    Feat: 지도 추가, 지도 에셋 추가, 마커사이즈 변경(extension)

---
+++ Projects/App/Sources/Feature/Map/View/MapView.swift
@@ -0,0 +1,56 @@
+//
+//  MapView.swift
+//  gambler
+//
+//  Created by daye on 1/25/24.
+//  Copyright © 2024 gambler. All rights reserved.
+//
+
+import SwiftUI
+import UIKit
+import KakaoMapsSDK
+import CoreLocation
+
+struct MapView: View {
+    @State var draw: Bool = false
+    @State private var userLatitude: Double = 37.402001
+    @State private var userLongitude: Double = 127.108678
+    @State var isShowingSheet: Bool = false
+    @State var detent: PresentationDetent = .medium
+    
+    var body: some View {
+        KakaoMapView(draw: $draw, userLatitude: $userLatitude, userLongitude: $userLongitude, isShowingSheet: $isShowingSheet, isMainMap: true)
+            .onAppear {
+                self.draw = true
+            }
+            .overlay {
+                Group {
+                    if isShowingSheet {
+                        FlotingView()
+                            .offset(y: 250)
+                    }
+                }
+            }
+        
+        /*.onDisappear(perform: {
+         self.draw = false
+         })*/
+            .edgesIgnoringSafeArea(.top)
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+
+    private struct FlotingView: View {
+        var body: some View{
+            HStack{
+                Text("FlotingView")
+            }.frame(width: 327, height: 132)
+                .background(Color.white)
+                .cornerRadius(16)
+        }
+    }
+    
+}
+
+#Preview {
+    KakaoMapView(draw: .constant(true), userLatitude: .constant(13.0000), userLongitude: .constant(13.0000), isShowingSheet: .constant(false), isMainMap: true)
+}
---
+++ Projects/App/Sources/Feature/Map/ViewModel/MapTestData.swift
@@ -0,0 +1,27 @@
+//
+//  MapTestData.swift
+//  gambler
+//
+//  Created by daye on 1/25/24.
+//  Copyright © 2024 gambler. All rights reserved.
+//
+
+import Foundation
+
+struct MarkerTestData {
+    var name: String
+    var longitude: Double
+    var latitude: Double
+}
+
+class MarkerTestDataManager {
+    var markerTestDatas: [MarkerTestData]
+    
+    init() {
+        markerTestDatas = [
+            MarkerTestData(name: "판교1", longitude: 127.1122214051127, latitude: 37.395815438352216),
+            MarkerTestData(name: "판교2", longitude: 127.11297786474694, latitude: 37.39568857499883),
+            MarkerTestData(name: "판교3", longitude: 127.11000802973668, latitude: 37.395889599947324)
+        ]
+    }
+}
