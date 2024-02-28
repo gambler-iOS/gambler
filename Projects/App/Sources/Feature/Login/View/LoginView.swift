400d3a014af184cdbe54e84e4f3a3837a8782e53
Sunghun Park <dfstem9@gmail.com>
2024-02-29 00:39:26 +0900

    Feat: LoginView 구현

---
+++ Projects/App/Sources/Feature/Login/View/LoginView.swift
@@ -0,0 +1,67 @@
+//
+//  LoginView.swift
+//  gambler
+//
+//  Created by 박성훈 on 2/29/24.
+//  Copyright © 2024 gambler. All rights reserved.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        VStack(spacing: .zero) {
+            HStack {
+                Text("문구 문구 문구문구문구")
+                    .font(.head1B)
+                    .foregroundStyle(Color.gray900)
+                    .frame(width: 166, height: 96)
+                Spacer()
+            }
+            .padding(.top, 123)
+            
+            Spacer()
+            
+            VStack(spacing: 16) {
+                Image("Kakao")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(height: 60)
+                    .onTapGesture {
+                        // 카카오 로그인
+                    }
+                
+                Image("Apple")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(height: 60)
+                    .onTapGesture {
+                        // 애플 로그인
+                    }
+                
+                Image("Google")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(height: 60)
+                    .onTapGesture {
+                        // 구글 로그인
+                    }
+            }
+            .padding(.bottom, 56)
+            
+            HStack {
+                Text("이미 가입하셨나요?")
+                Text("로그인")
+                    .underline()
+            }
+            .foregroundStyle(Color.gray600)
+            .padding(.bottom, 24)
+            
+        }
+        .padding(.horizontal, 24)
+    }
+}
+
+#Preview {
+    LoginView()
+}
---
+++ Projects/App/Sources/Feature/Login/ViewModel/LoginViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  LoginViewModel.swift
+//  gambler
+//
+//  Created by 박성훈 on 2/29/24.
+//  Copyright © 2024 gambler. All rights reserved.
+//
+
+import Foundation
