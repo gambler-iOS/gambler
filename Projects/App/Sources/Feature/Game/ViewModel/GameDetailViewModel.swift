fa649a0a44c20f0037b1365298384fe6249ae8f0
ahnhymy <licors214@gmail.com>
2024-03-08 19:20:14 +0900

    Fix: Game Model 변경, dummyGameList 데이터 추가, 게임관련 viewModel dummyData 코드 변경
    Github issue #71

--- Projects/App/Sources/Feature/Game/ViewModel/GameDetailViewModel.swift
+++ Projects/App/Sources/Feature/Game/ViewModel/GameDetailViewModel.swift
@@ -22,26 +22,8 @@ final class GameDetailViewModel: ObservableObject {
     func generateDummyData() {
         for num in 1...3 {
             reviews.append(Review.dummyGameReview)
-            similarGenreGames.append(
-                Game(id: UUID().uuidString, gameName: "game\(num)",
-                     gameImage: "https://weefun.co.kr/shopimages/weefun/007009000461.jpg?1596805186",
-                     descriptionContent: "게임 상세 설명",
-                     descriptionImage: ["https://boardm5.godohosting.com/goods/2024/02/dt01.png"],
-                     gameLink: "link\(num)", createdDate: Date(),
-                     reviewCount: 1 + num, reviewRatingAverage: 3.5 + (0.1 * Double(num)),
-                     gameIntroduction: GameIntroduction(difficulty: 3.1, minPlayerCount: 2 + num,
-                                                        maxPlayerCount: 4 + num, playTime: 2 + num,
-                                                        genre: .fantasy)))
-            similarPlayerGames.append(
-                Game(id: UUID().uuidString, gameName: "game1\(num)",
-                     gameImage: "https://weefun.co.kr/shopimages/weefun/007009000461.jpg?1596805186",
-                     descriptionContent: "게임 상세 설명",
-                     descriptionImage: ["https://boardm5.godohosting.com/goods/2024/02/dt01.png"],
-                     gameLink: "link\(num)", createdDate: Date(),
-                     reviewCount: 1 + num, reviewRatingAverage: 3.5 + (0.1 * Double(num)),
-                     gameIntroduction: GameIntroduction(difficulty: 3.1, minPlayerCount: 2 + num,
-                                                        maxPlayerCount: 4 + num, playTime: 2 + num,
-                                                        genre: .fantasy)))
+            similarGenreGames = Game.dummyGameList
+            similarPlayerGames = Game.dummyGameList
         }
     }
 }
--- Projects/App/Sources/Feature/Game/ViewModel/GameListViewModel.swift
+++ Projects/App/Sources/Feature/Game/ViewModel/GameListViewModel.swift
@@ -17,17 +17,6 @@ final class GameListViewModel: ObservableObject {
     }
 
     func generateDummyData() {
-        let gameImageUrl: String =
-        "https://weefun.co.kr/shopimages/weefun/007009000461.jpg?1596805186"
-        for num in 1...5 {
-            games.append(Game(id: UUID().uuidString, gameName: "game\(num)", gameImage: gameImageUrl,
-                              descriptionContent: "게임 상세 설명",
-                              descriptionImage: ["https://boardm5.godohosting.com/goods/2024/02/dt01.png"],
-                              gameLink: "link\(num)", createdDate: Date(),
-                              reviewCount: 1 + num, reviewRatingAverage: 3.5 + (0.1 * Double(num)),
-                              gameIntroduction: GameIntroduction(difficulty: 3.1, minPlayerCount: 2 + num,
-                                                                 maxPlayerCount: 4 + num, playTime: 2 + num,
-                                                                 genre: .fantasy)))
-        }
+        games = Game.dummyGameList
     }
 }
