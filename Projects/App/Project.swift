bc43267ede107f155c91269a3924021235107902
da-hye0 <60743139+da-hye0@users.noreply.github.com>
2024-03-14 23:24:05 +0900

    fix: tuist 설정 변경

--- Projects/App/Project.swift
+++ Projects/App/Project.swift
@@ -16,7 +16,7 @@ let project = Project.makeModule(
         .remote(url: "https://github.com/firebase/firebase-ios-sdk", requirement: .exact("10.22.1")),
         .remote(url: "https://github.com/Alamofire/Alamofire", requirement: .upToNextMajor(from: "5.8.1")),
         .remote(url: "https://github.com/onevcat/Kingfisher", requirement: .upToNextMajor(from: "7.0.0")),
-        .remote(url: "https://github.com/kakao-mapsSDK/KakaoMapsSDK-SPM.git", requirement: .upToNextMajor(from: "2.9.0")),
+        .remote(url: "https://github.com/kakao-mapsSDK/KakaoMapsSDK-SPM.git", requirement: .upToNextMinor(from: "2.9.0")),
         .remote(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", requirement: .upToNextMajor(from: "4.0.0")),
         .remote(url: "https://github.com/kakao/kakao-ios-sdk", requirement: .upToNextMajor(from: "2.20.0")),
                 .remote(url: "https://github.com/google/GoogleSignIn-iOS", requirement: .upToNextMajor(from: "7.0.0"))
